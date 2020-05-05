46af5b4fbfe16544f607fc01d8047c5f5f5ca899
曾志杰 <axi@cengzhijiedeiMac.local>
2020-05-05 16:47:32 +0800

    feat: 增加用户事件

--- src/app.js
+++ src/app.js
@@ -7,14 +7,20 @@ const server = http.createServer(app.callback()).listen(3000, () => {
 })
 
 const io = require('socket.io')(server)
+let users = []
 
 io.on('connection', socket => {
-  console.log(`[User] connected: ${socket}`)
+  console.log(`[User] connected`)
   socket.on('save', data => {
-    console.log(data)
     io.emit('save', data)
   })
+  socket.on('user_join', data => {
+    users = users.concat(data)
+    io.emit('user_join', { users })
+  })
   socket.on('disconnect', (reason) => {
+    users = users.filter(v => v.id !== socket.id)
+    io.emit('user_quit', { users })
     console.log(`[User] disconnected: ${reason}`)
   });
 })
